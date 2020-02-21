5dfa4b8083b0bd82c96205d76cf54b1d88b5a035
David <dasmartins1234@gmail.com>
2020-02-21 14:50:09 +0000

    21 - PHP Storm

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,13 @@ Route::get('/home', 'SiteController@index')->name('/');
 
 Route::group(['middleware' => ['auth']],function(){
     Route::group(['middleware' => ['is_admin']],function(){
-        
+
         Route::get('/m', 'SiteController@menu');
         Route::resource('user','UserController');
         Route::resource('utente','UtenteController');
         Route::resource('medica','MedicamentosController');
         Route::resource('servico','ServicoController');
-        Route::resource('estado','EstadoController'); 
+        Route::resource('estado','EstadoController');
         Route::resource('visita','VisitaController');
         Route::get('/pusers','UserController@pesquisar');
         Route::get('/ptt','UtenteController@pesquisar');
@@ -32,10 +32,11 @@ Route::group(['middleware' => ['auth']],function(){
         Route::get('/pserv','ServicoController@pesquisar');
         Route::get('/pest','EstadoController@pesquisar');
         Route::get('/pvis','VisitaController@pesquisar');
-    }); 
+    });
     Route::get('/horario','SiteController@horario');
+    Route::resource('event','EventController');
 
 
 });
 
-  
+

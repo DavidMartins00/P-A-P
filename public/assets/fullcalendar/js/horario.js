df1fe2c574b02688b7bb4c29cb9a28bf559a9f44
David <dasmartins1234@gmail.com>
2020-02-27 15:17:17 +0000

    Horario 27

---
+++ public/assets/fullcalendar/js/horario.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var Calendar = FullCalendar.Calendar;
+    var Draggable = FullCalendarInteraction.Draggable
+
+    /* initialize the external events
+    -----------------------------------------------------------------*/
+
+    var containerEl = document.getElementById('external-events-list');
+    new Draggable(containerEl, {
+        itemSelector: '.fc-event',
+        eventData: function(eventEl) {
+            return {
+                title: eventEl.innerText.trim()
+            }
+        }
+    });
+
+    /* initialize the calendar
+    -----------------------------------------------------------------*/
+
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new Calendar(calendarEl, {
+        plugins: [ 'interaction', 'dayGrid', 'timeGrid', 'list' ],
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+        },
+        locale: 'pt',
+        navLinks: true,
+        eventLimit: true,
+        droppable: false, // this allows things to be dropped onto the calendar
+
+        events: routeEvents('routeLoadEvents'),
+    });
+    calendar.render();
+
+});

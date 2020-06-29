75472642e8bbda64b3ecad79eba815b55fe5f045
David <dasmartins1234@gmail.com>
2020-06-29 20:57:13 +0100

    Projeto final

--- resources/views/site/mail.blade.php
+++ resources/views/site/mail.blade.php
@@ -1,8 +1,9 @@
 <html>
     <body>
-        <p>Olá {{ $nome }}!</p>
+        <p>Olá!</p>
         <br>
-        <p>O utilizador {{$nome}} com o email {{$email}} e contacto {{$contacto}} enviou-lhe a seguinte mensaguem {{$msg}}</p>
+        <p>O utilizador {{$nome}} com o email {{$email}} e contacto {{$contacto}} enviou-lhe a seguinte mensaguem:</p><br>
+        <p>{{$msg}}</p>
         <br>
     </body>
 </html>
